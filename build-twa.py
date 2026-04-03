580d9b88c5d7f2684712ab6abdc8f2981bf0c36a
dumitriualx-lang <dumitriu.alx@gmail.com>
2026-04-03 10:43:55 +0300

    Update build-twa.py

--- build-twa.py
+++ build-twa.py
@@ -18,31 +18,22 @@ def write(path, content):
     with open(path, "w") as f:
         f.write(content)
 
-# 1. settings.gradle
-write(os.path.join(ROOT, "settings.gradle"), """
-pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
-}
-rootProject.name = "twa"
-include(":app")
-""")
+# 1. Project Configuration Files
+write(os.path.join(ROOT, "settings.gradle"), "include ':app'")
+write(os.path.join(ROOT, "gradle.properties"), "android.useAndroidX=true\nandroid.enableJetifier=true")
 
-# 2. root build.gradle
+# 2. Root build.gradle (Fixes the repository error)
 write(os.path.join(ROOT, "build.gradle"), """
 buildscript {
     repositories { google(); mavenCentral() }
     dependencies { classpath "com.android.tools.build:gradle:8.2.0" }
 }
+allprojects {
+    repositories { google(); mavenCentral() }
+}
 """)
 
-# 3. gradle.properties
-write(os.path.join(ROOT, "gradle.properties"), "android.useAndroidX=true\nandroid.enableJetifier=true")
-
-# 4. app/build.gradle
+# 3. App build.gradle
 write(os.path.join(APP, "build.gradle"), f"""
 plugins {{ id 'com.android.application' }}
 android {{
@@ -52,8 +43,8 @@ android {{
         applicationId "{PKG}"
         minSdk 21
         targetSdk 34
-        versionCode 45
-        versionName "1.4.5"
+        versionCode 50
+        versionName "1.5.0"
         manifestPlaceholders = [
             hostName: "{HOST}",
             defaultUrl: "{START_URL}",
@@ -81,9 +72,9 @@ dependencies {{
 }}
 """)
 
-# 5. AndroidManifest.xml
+# 4. AndroidManifest.xml (Android 14 Ready)
 write(os.path.join(MAIN, "AndroidManifest.xml"), """<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android" xmlns:tools="http://tools.android.com/apk/res/android">
+<manifest xmlns:android="http://schemas.android.com/apk/res/android" xmlns:tools="http://schemas.android.com/tools">
     <queries><intent><action android:name="android.support.customtabs.action.CustomTabsService" /></intent></queries>
     <uses-permission android:name="android.permission.INTERNET"/>
     <application android:label="${launcherName}" android:icon="@mipmap/ic_launcher" android:theme="@android:style/Theme.Translucent.NoTitleBar">
@@ -102,7 +93,21 @@ write(os.path.join(MAIN, "AndroidManifest.xml"), """<?xml version="1.0" encoding
 </manifest>
 """)
 
-# 6. Basic Resources (prevents linking errors)
-write(os.path.join(RES, "values", "colors.xml"), """<?xml version="1.0" encoding="utf-8"?><resources><color name="colorPrimary">#1D9E75</color></resources>""")
+# 5. GENERATE AUTOMATIC ICON (Fixes the 404/Missing Resource error)
+# This creates a green placeholder adaptive icon so the build always passes
+write(os.path.join(RES, "values", "colors.xml"), """<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <color name="ic_launcher_background">#1D9E75</color>
+</resources>""")
 
-print("Project generated successfully.")
+write(os.path.join(RES, "mipmap-anydpi-v26", "ic_launcher.xml"), """<?xml version="1.0" encoding="utf-8"?>
+<adaptive-icon xmlns:android="http://schemas.android.com/apk/res/android">
+    <background android:drawable="@color/ic_launcher_background"/>
+    <foreground>
+        <inset android:inset="25%">
+            <path android:fillColor="#FFFFFF" android:pathData="M12,2L4.5,20.29L5.21,21L12,18L18.79,21L19.5,20.29L12,2Z"/>
+        </inset>
+    </foreground>
+</adaptive-icon>""")
+
+print("Project generated successfully with built-in resources.")
