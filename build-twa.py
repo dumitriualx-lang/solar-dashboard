f4701521faac0cf8158ce9b58a59ff76456424ce
dumitriualx-lang <dumitriu.alx@gmail.com>
2026-04-26 09:17:58 +0300

    Update build-twa.py

--- build-twa.py
+++ build-twa.py
@@ -50,8 +50,8 @@ android {
         applicationId "%s"
         minSdk 21
         targetSdk 35
-        versionCode 2
-        versionName "1.0.1"
+        versionCode 3
+        versionName "1.0.2"
         manifestPlaceholders = [
             hostName:     "dumitriualx-lang.github.io",
             defaultUrl:   "https://dumitriualx-lang.github.io/solar-dashboard/",
