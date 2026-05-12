8828a275885bb8902abfa3b8adff0bf010006062
dumitriualx-lang <dumitriu.alx@gmail.com>
2026-05-12 08:30:58 +0300

    Update build-twa.py

--- build-twa.py
+++ build-twa.py
@@ -10,7 +10,7 @@ VERSION_NAME = "1.2.0"
 if os.path.exists(_vfile):
     with open(_vfile) as _vf: VERSION_CODE = int(_vf.read().strip()) + 1
 else:
-    VERSION_CODE = 6
+    VERSION_CODE = 8
 with open(_vfile, "w") as _vf: _vf.write(str(VERSION_CODE))
 print(f"Build: versionCode={VERSION_CODE}  versionName={VERSION_NAME}")
 
